458ebd8fc91b805da4e2a002ad9362fcef1cdf61
jonrib <47106327+jonrib@users.noreply.github.com>
2025-01-05 20:38:04 +0200

    Added GitHub authentication

--- Program.cs
+++ Program.cs
@@ -1,31 +1,34 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Primitives;
 using Microsoft.IdentityModel.Tokens;
 using OAuthExample;
 using OAuthExample.Persistence;
 using OpenIddict.Abstractions;
+using OpenIddict.Client.AspNetCore;
+using OpenIddict.Client.WebIntegration;
 using OpenIddict.Server.AspNetCore;
 using OpenIddict.Validation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(options => {options.DefaultScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;});
+builder.Services.AddAuthentication(options => {options.DefaultScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;})
+    .AddCookie("ExternalLogin", options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+        options.SlidingExpiration = false;
+        options.Cookie.MaxAge = TimeSpan.FromMinutes(1);
+    });
 builder.Services.AddAuthorization(opt =>
 {
     opt.InvokeHandlersAfterFailure = false;
 });
 
-// Register the worker responsible for seeding the database.
-// Note: in a real world application, this step should be part of a setup script.
+// Worker for seeding DB
 builder.Services.AddHostedService<Worker>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -34,67 +37,65 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseOpenIddict();
 });
 
+//OpenIddict stuffs
 builder.Services.AddOpenIddict()
-
-    // Register the OpenIddict core components.
     .AddCore(options =>
     {
-        // Configure OpenIddict to use the Entity Framework Core stores and models.
-        // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
         options.UseEntityFrameworkCore()
             .UseDbContext<AppDbContext>();
     })
-
-    // Register the OpenIddict server components.
+    .AddClient(options =>
+    {
+        options.AllowAuthorizationCodeFlow();
+        
+        // For DEV only
+        options.AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+        
+        options.UseAspNetCore()
+            .EnableRedirectionEndpointPassthrough();
+        
+        options.UseSystemNetHttp();
+        
+        options.SetStateTokenLifetime(TimeSpan.FromMinutes(1));
+        
+        options.UseWebProviders()
+            .AddGitHub(options =>
+            {
+                options.SetClientId("Ov23lize03OCXfRuoFtj")
+                    .SetClientSecret("3a4ef54a546f234375de1f27cee5c7c60eba9575")
+                    .SetRedirectUri("callback/login/github");
+            });
+    })
     .AddServer(options =>
     {
-        // Enable the authorization, logout, token and userinfo endpoints.
         options.SetAuthorizationEndpointUris("connect/authorize")
             .SetTokenEndpointUris("connect/token")
             .SetUserInfoEndpointUris("connect/userinfo");
 
-        // Mark the "email", "profile" and "roles" scopes as supported scopes.
-        options.RegisterScopes(OpenIddictConstants.Scopes.Email, OpenIddictConstants.Scopes.Profile,
-            OpenIddictConstants.Scopes.Roles);
-
-        // Note: the sample uses the code and refresh token flows but you can enable
-        // the other flows if you need to support implicit, password or client credentials.
+        // Mark the "email" scopes as supported scope.
+        options.RegisterScopes(OpenIddictConstants.Scopes.Email);
+        
         options.AllowAuthorizationCodeFlow()
             .RequireProofKeyForCodeExchange()
             .AllowRefreshTokenFlow();
-
-        // Register the signing and encryption credentials.
+        
         options.AddDevelopmentEncryptionCertificate()
             .AddDevelopmentSigningCertificate();
 
         options.DisableAccessTokenEncryption();
         
-        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
         options.UseAspNetCore()
             .EnableAuthorizationEndpointPassthrough()
             .EnableTokenEndpointPassthrough();
     })
-
-    // Register the OpenIddict validation components.
     .AddValidation(options =>
     {
-        // Import the configuration from the local OpenIddict server instance.
         options.UseLocalServer();
-
-        // Register the ASP.NET Core host.
         options.UseAspNetCore();
     });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
@@ -117,71 +118,64 @@ app.MapGet("/weatherforecast", () =>
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
-app.MapGet("/connect/authorize", Authorize)
-    .WithOpenApi();
-app.MapPost("/connect/authorize", Authorize)
-    .WithOpenApi();
+app.MapGet("/connect/authorize", Authorize);
+app.MapPost("/connect/authorize", Authorize);
+app.MapGet("/callback/login/{provider}", LogInCallback);
+app.MapPost("/callback/login/{provider}", LogInCallback);
+app.MapPost("/connect/token", Exchange);
+
+async Task LogInCallback(HttpContext context)
+{
+    var result = await context.AuthenticateAsync(OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
+    
+    if (result.Principal is not { Identity.IsAuthenticated: true })
+    {
+        throw new InvalidOperationException("The external authorization data cannot be used for authentication.");
+    }
+    
+    var identity = new ClaimsIdentity(authenticationType: "ExternalLogin");
+    
+    identity
+        .SetClaim("login", result.Principal.GetClaim("login"))
+            .SetClaim(ClaimTypes.Email, result.Principal.GetClaim(ClaimTypes.Email))
+            .SetClaim(ClaimTypes.Name, result.Principal.GetClaim(ClaimTypes.Name))
+            .SetClaim(ClaimTypes.NameIdentifier, result.Principal.GetClaim(ClaimTypes.NameIdentifier));
+    
+    var properties = new AuthenticationProperties(result.Properties.Items)
+    {
+        RedirectUri = result.Properties.RedirectUri ?? "/"
+    };
+    
+    properties.StoreTokens(result.Properties.GetTokens().Where(token => token.Name is
+        OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken   or
+        OpenIddictClientAspNetCoreConstants.Tokens.BackchannelIdentityToken or
+        OpenIddictClientAspNetCoreConstants.Tokens.RefreshToken));
+    
+    await context.SignInAsync("ExternalLogin", new ClaimsPrincipal(identity), properties);
+}
 
 async Task Authorize(HttpContext context)
 {
     var request = context.GetOpenIddictServerRequest() ??
                   throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
     
-    var result = await context.AuthenticateAsync();
-    if (!result.Succeeded || request.HasPromptValue(OpenIddictConstants.PromptValues.Login) ||
-        (request.MaxAge != null && result.Properties?.IssuedUtc != null &&
-         DateTimeOffset.UtcNow - result.Properties.IssuedUtc > TimeSpan.FromSeconds(request.MaxAge.Value)))
+    var result = await context.AuthenticateAsync("ExternalLogin");
+    if (!result.Succeeded)
     {
-        // If the client application requested promptless authentication,
-        // return an error indicating that the user is not logged in.
-        if (request.HasPromptValue(OpenIddictConstants.PromptValues.None))
-        {
-            await context.ForbidAsync(
-                OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
-                properties: new AuthenticationProperties(new Dictionary<string, string>
-                {
-                    [OpenIddictServerAspNetCoreConstants.Properties.Error] = OpenIddictConstants.Errors.LoginRequired,
-                    [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] = "The user is not logged in."
-                }!));
-            return;
-        }
-
-        // To avoid endless login -> authorization redirects, the prompt=login flag
-        // is removed from the authorization request payload before redirecting the user.
-        var prompt = string.Join(" ", request.GetPromptValues().Remove(OpenIddictConstants.PromptValues.Login));
-
-        var parameters = context.Request.HasFormContentType
-            ? context.Request.Form.Where(parameter => parameter.Key != OpenIddictConstants.Parameters.Prompt).ToList()
-            : context.Request.Query.Where(parameter => parameter.Key != OpenIddictConstants.Parameters.Prompt).ToList();
-
-        parameters.Add(KeyValuePair.Create(OpenIddictConstants.Parameters.Prompt, new StringValues(prompt)));
-
-        // For scenarios where the default challenge handler configured in the ASP.NET Core
-        // authentication options shouldn't be used, a specific scheme can be specified here.
-        await context.ChallengeAsync(new AuthenticationProperties
-        {
-            RedirectUri = context.Request.PathBase + context.Request.Path + QueryString.Create(parameters)
-        });
+        await context.ChallengeAsync(OpenIddictClientWebIntegrationConstants.Providers.GitHub);
         return;
     }
-
-
-    // Create the claims-based identity that will be used by OpenIddict to generate tokens.
+    
     var identity = new ClaimsIdentity(
         authenticationType: TokenValidationParameters.DefaultAuthenticationType,
         nameType: OpenIddictConstants.Claims.Name,
         roleType: OpenIddictConstants.Claims.Role);
-
-    // Add the claims that will be persisted in the tokens.
-    identity.SetClaim(OpenIddictConstants.Claims.Subject, "Test")
-        .SetClaim(OpenIddictConstants.Claims.Email, "Test")
-        .SetClaim(OpenIddictConstants.Claims.Name, "Test")
-        .SetClaim(OpenIddictConstants.Claims.PreferredUsername, "test")
-        .SetClaims(OpenIddictConstants.Claims.Role, ["test"]);
-
-    // Note: in this sample, the granted scopes match the requested scope
-    // but you may want to allow the user to uncheck specific scopes.
-    // For that, simply restrict the list of scopes before calling SetScopes.
+    
+    identity.SetClaim(OpenIddictConstants.Claims.Subject, result.Principal.GetClaim("login"))
+        .SetClaim(OpenIddictConstants.Claims.Email, result.Principal.GetClaim(ClaimTypes.Email))
+        .SetClaim(OpenIddictConstants.Claims.Name, result.Principal.GetClaim(ClaimTypes.Name));
+    
+    // In reality should be limited...
     identity.SetScopes(request.GetScopes());
     
     var principal = new ClaimsPrincipal(identity);
@@ -190,9 +184,6 @@ async Task Authorize(HttpContext context)
 
     await context.SignInAsync(ticket.AuthenticationScheme, ticket.Principal, ticket.Properties);
 }
-
-app.MapPost("/connect/token", Exchange)
-    .WithOpenApi();
 
 async Task Exchange(HttpContext context)
 {
@@ -211,13 +202,10 @@ async Task Exchange(HttpContext context)
 
         // Override the user claims present in the principal in case they
         // changed since the authorization code/refresh token was issued.
-        identity.SetClaim(OpenIddictConstants.Claims.Subject, "test")
-            .SetClaim(OpenIddictConstants.Claims.Email, "test")
-            .SetClaim(OpenIddictConstants.Claims.Name, "test")
-            .SetClaim(OpenIddictConstants.Claims.PreferredUsername, "test")
-            .SetClaims(OpenIddictConstants.Claims.Role, ["test"]);
-
-        // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
+        identity.SetClaim(OpenIddictConstants.Claims.Subject, result.Principal.GetClaim(OpenIddictConstants.Claims.Subject))
+            .SetClaim(OpenIddictConstants.Claims.Email, result.Principal.GetClaim(OpenIddictConstants.Claims.Email))
+            .SetClaim(OpenIddictConstants.Claims.Name, result.Principal.GetClaim(OpenIddictConstants.Claims.Name));
+        
         var principal = new ClaimsPrincipal(identity);
     
         var ticket = new AuthenticationTicket(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
@@ -228,6 +216,17 @@ async Task Exchange(HttpContext context)
 
     throw new InvalidOperationException("The specified grant type is not supported.");
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseDeveloperExceptionPage();
+app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
